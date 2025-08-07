87c06eea1397626a43956ff9444fe389c0bfdf16
Alen C Saji <56498832+AlenCSaji@users.noreply.github.com>
2025-08-07 17:02:20 -0400

    Add files via upload

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InventoryTracker.Models
+﻿namespace InventoryTracker.Models
 {
     public class Item
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public int Quantity { get; set; }
     }
 }
+
